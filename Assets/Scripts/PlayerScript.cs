f3b24ef8d1c4a877c21180b5a4ae8215725d3b16
raphael frank <04.raphael.frank@gmail.com>
2023-03-13 20:55:15 +0100

    Player Input

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Input
+    private PlayerInput playerInput;
+    private bool canMove;
+    //DreamShift
+    private bool canDreamShift;
+    //Abilities
+    private bool hasGlide;
+    private bool hasDoubleJump;
+    
+    private void Awake()
     {
-        
+        playerInput = new PlayerInput();
+
+        playerInput.Movement.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        playerInput.Movement.Move.canceled += ctx => Move(ctx.ReadValue<Vector2>());
+        playerInput.Movement.Jump.performed += ctx => Jump();
+        playerInput.Movement.Attack.performed += ctx => Attack();
+        playerInput.Movement.Interact.performed += ctx => Interact();
+        playerInput.Movement.DreamShift.performed += ctx => DreamShift();
+        playerInput.Movement.Glide.performed += ctx => Glide(true);
+        playerInput.Movement.Glide.canceled += ctx => Glide(false);
+
+        playerInput.Movement.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move(Vector2 value)
     {
-        
+        if(!canMove) return;
+        Debug.Log(value);
+    }
+
+    private void Jump()
+    {
+        if(!canMove) return;
+        Debug.Log("Jump");
+    }
+
+    private void Attack()
+    {
+        if(!canMove) return;
+        Debug.Log("Attack");
+    }
+
+    private void Interact()
+    {
+        Debug.Log("Interact");
+    }
+
+    private void DreamShift()
+    {
+        if(!canDreamShift) return;
+        if(!canMove) return;
+        Debug.Log("DreamShift");
+    }
+
+    private void Glide(bool pressed)
+    {
+        if(!hasGlide) return;
+        if(!canMove) return;
+        Debug.Log("Glide");
     }
 }
