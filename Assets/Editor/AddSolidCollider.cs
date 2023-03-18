9a553b1451005595282e80446afae8afbb478ce1
Werner Richter <werner.richter@tum.de>
2023-03-18 19:15:40 +0100

    test

--- Assets/Editor/AddSolidCollider.cs
+++ Assets/Editor/AddSolidCollider.cs
@@ -7,7 +7,7 @@ public class AddSolidCollider
     /*[MenuItem("Collider Mode/Add Collider %g")]
     private static void SwitchNightmare()
     {
-        GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Sprites/Forest/ColliderHS.prefab", typeof(GameObject));
+        GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Sprites/Forest/ColliderHL2.prefab", typeof(GameObject));
         GameObject.Instantiate(prefab, Selection.transforms[0]);
     }*/
 }
